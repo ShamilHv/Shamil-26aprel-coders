d00031d0f6bdc9619296cd30ff536298216ccc6f
ShamilHv <samilheyderov2003@gmail.com>
2023-05-07 15:20:43 +0400

    update

---
+++ src/az/coders/project/Polindrome.java
@@ -0,0 +1,20 @@
+package az.coders.project;
+
+import java.util.Scanner;
+
+public class Polindrome {
+    public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a word");
+        String word= scan.nextLine();
+        char [] ch=new char[word.length()-1];
+        for(int i=0;i<word.length();i++){
+          word.substring(1,2);
+        }
+
+    }
+
+
+
+}
