5fe98cd0ed545d900597cfb9477c692ea6fad2a9
ShamilHv <samilheyderov2003@gmail.com>
2023-05-07 14:19:09 +0400

    update

---
+++ src/az/coders/project/Fibonacci.java
@@ -0,0 +1,33 @@
+package az.coders.project;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean check = false;
+        int arr[] = new int[5];
+        System.out.println("Enter elements of array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 1) {
+                if (arr[i] == arr[i - 1] + arr[i - 2]) {
+                    check = true;
+                } else {
+                    check = false;
+                }
+            }
+        }
+        if (check) {
+            System.out.println("it is fibonacci");
+        } else {
+            System.out.println("it is not fibonacci");
+        }
+
+
+    }
+}
